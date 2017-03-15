16da1ceb4e3e9d6f9d2d685b386ae664236c3f2d
Corneliousm808 <corneliousmooring@gmail.com>
2017-03-15 09:13:25 -1000

    yeah

---
+++ app.js
@@ -0,0 +1,43 @@
+var jsonDataBody = [
+  {
+    "header":"h1","h4"
+  },
+  {
+    "div"; {
+      "class": "aside",
+      ["h5", "nav"]
+    }
+  },
+];
+
+  var jsonDataBodyEntryOne = [
+  {
+    "div": {
+      "id":"first",
+      "class":"content",
+      ["h2","h6","h3"]
+    }
+  }
+]
+
+var simplifiedJson = [
+  {
+    "journalEntry": "1",
+    "date": "August 15, 2016",
+    "title":"Why I Took This Class",
+    "content": "I am interested in learning how to do the basics of coding. Also because my engineering teacher said that in the near future there will be a high demand of engineers needeing to know how to code. So when there was that meeting in the library about a new coding class, I saw an opportunity and I took it.",
+    "article": [
+    "To know the basics of coding. That way if i apply for an engineering job, I can put that I know how to code and maybe I can get a better job. Also that when coding is a requirement for coollege engineering, I wont have to take that class because i already took it in high school. So with that I can have a free period, or fill it in whit something that can benefit me."
+    ]
+  },
+  {
+    "journalEntry": "2",
+    "date": "August 24, 2016",
+    "title":"Difference Between UX And UI"
+    "content":"UX stands for user exprience. User experience is how the user uses the design of the product. Ussually the user will be lookiing at the content an Show the design is layed out. UI stands for user interface. User interface is how the user will interact with the computer."
+  },
+  {
+    "journalEntry":"3",
+    "date"
+  }
+]
